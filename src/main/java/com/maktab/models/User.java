4140d61b9eea99461f179fba3323c48be3731f0d
Bahar Zolfaghari <baharzolfaghari2000@gmail.com>
2021-03-25 12:27:58 +0430

    Create user class for common properties between customer, clerk and manager

---
+++ src/main/java/com/maktab/models/User.java
@@ -0,0 +1,63 @@
+package com.maktab.models;
+
+import javax.persistence.*;
+
+@Entity
+@Inheritance(strategy= InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(
+        name = "Discriminator", discriminatorType = DiscriminatorType.STRING
+)
+@DiscriminatorValue(value="User")
+public class User {
+    @Id
+    @Column(name = "Username")
+    private String username;
+    @Column(name = "Password", nullable = false)
+    private String password;
+    @Column(name = "First Name", length = 15)
+    private String firstName;
+    @Column(name = "Last Name", length = 25)
+    private String lastName;
+
+    public User() {
+    }
+
+    public User(String username, String password, String firstName, String lastName) {
+        this.username = username;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
