9330358d4dda3081c9a196fc1359531b1830ccb3
Bahar Zolfaghari <baharzolfaghari2000@gmail.com>
2021-03-29 11:47:37 +0430

    Add repository for customer

---
+++ src/main/java/com/maktab/repositories/CustomerRepository.java
@@ -0,0 +1,30 @@
+package com.maktab.repositories;
+
+import com.maktab.models.Customer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CustomerRepository {
+    private static final List<Customer> customers = new ArrayList<>();
+
+    public void addCustomer(Customer customer) {
+        customers.add(customer);
+    }
+
+    public void addCustomer(List<Customer> customers) {
+        CustomerRepository.customers.addAll(customers);
+    }
+
+    public static List<Customer> getCustomers() {
+        return customers;
+    }
+
+    public boolean existsUsername(String username) {
+        return customers.stream().filter(customer -> customer.getUsername().equals(username)).count() == 1;
+    }
+
+    public boolean existsEmail(String email) {
+        return customers.stream().filter(customer -> customer.getEmail().equals(email)).count() == 1;
+    }
+}
