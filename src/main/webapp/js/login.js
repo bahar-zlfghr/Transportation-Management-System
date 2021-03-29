c0799c1543658bf6c30714cdbfaa38430de30a3f
Bahar Zolfaghari <baharzolfaghari2000@gmail.com>
2021-03-29 12:51:08 +0430

    Edit error message

--- src/main/webapp/js/login.js
+++ src/main/webapp/js/login.js
@@ -4,16 +4,20 @@ function validateLoginForm() {
 
     if(!validateUsername(username)){
         document.getElementById('empty-error-username').innerHTML = " * Username must not be empty";
+        document.getElementById('incorrect-username-or-password').innerHTML = "";
     }
     else {
         document.getElementById('empty-error-username').innerHTML = "";
+        document.getElementById('incorrect-username-or-password').innerHTML = "";
     }
 
     if(!validatePassword(password)) {
         document.getElementById('error-password').innerHTML = " * Password must not be empty";
+        document.getElementById('incorrect-username-or-password').innerHTML = "";
     }
     else {
         document.getElementById('error-password').innerHTML = "";
+        document.getElementById('incorrect-username-or-password').innerHTML = "";
     }
 
     if(!validateUsername(username) || !validatePassword(password)) {
