af7f8770c8dbdd745cd7c072052cafc2d2b14758
Bahar Zolfaghari <baharzolfaghari2000@gmail.com>
2021-03-30 11:59:36 +0430

    Add session and save customer and new delivery in attribute session

--- src/main/java/com/maktab/servlets/LoginServlet.java
+++ src/main/java/com/maktab/servlets/LoginServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Optional;
@@ -17,8 +18,6 @@ import java.util.Optional;
 public class LoginServlet extends HttpServlet {
     private static final CustomerRepository customerRepository = new CustomerRepository();
     private static final CustomerDao customerDao = new CustomerDao();
-
-
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println(" # Login servlet");
@@ -32,18 +31,59 @@ public class LoginServlet extends HttpServlet {
 
         Optional<Customer> optionalCustomer = customerRepository.findCustomerByUsernameAndPassword(username, password);
 
-        if (optionalCustomer.isPresent()) {
+        if (username == null) {
+            System.out.println("# 1");
+            HttpSession session = request.getSession(false);
+            if (session != null && session.getAttribute("username") != null) {
+                if (optionalCustomer.isPresent()) {
+                    System.out.println("# 2");
+                    Customer customer = optionalCustomer.get();
+                    request.getRequestDispatcher("customer_menu.html").include(request, response);
+                    writer.println("<script>");
+                    writer.println("document.getElementById('welcome-message').innerHTML = \" Welcome " + customer.getFirstName() + " " + customer.getLastName() + "\";");
+                }
+                else {
+                    System.out.println("# 3");
+                    request.getRequestDispatcher("login.html").include(request, response);
+                    writer.println("<script>");
+                    writer.println("document.getElementById('incorrect-username-or-password').innerHTML = \"* Username or password is incorrect\";");
+                }
+                writer.println("</script>");
+            }
+            else {
+                System.out.println("# 4");
+                writer.println("<script>");
+                writer.println("alert(\"* You must login first, Please login\";");
+                writer.println("</script>");
+
+                request.getRequestDispatcher("login.html").include(request, response);
+            }
+        }
+        else if (optionalCustomer.isPresent()) {
+            System.out.println("# 5");
             Customer customer = optionalCustomer.get();
+
+            HttpSession session = request.getSession();
+            session.setAttribute("username", username);
+            session.setAttribute("password", password);
+            session.setAttribute("firstName", customer.getFirstName());
+            session.setAttribute("lastName", customer.getLastName());
+
             request.getRequestDispatcher("customer_menu.html").include(request, response);
+
             writer.println("<script>");
             writer.println("document.getElementById('welcome-message').innerHTML = \" Welcome " + customer.getFirstName() + " " + customer.getLastName() + "\";");
+            writer.println("</script>");
         }
         else {
-            request.getRequestDispatcher("login.html").include(request, response);
+            System.out.println("# 6");
             writer.println("<script>");
-            writer.println("document.getElementById('incorrect-username-or-password').innerHTML = \"* Username or password is incorrect\";");
+            writer.println("alert(\"* You must login first, Please login\";");
+            writer.println("</script>");
+
+            request.getRequestDispatcher("login.html").include(request, response);
         }
-        writer.println("</script>");
+
         writer.close();
     }
 
